a40b4f5e026a6c69b57984cdc082c69e9e066742
meekranjer <logicism81@gmail.com>
2023-01-21 14:59:56 -0500

    Update

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -17,5 +17,5 @@ export default function handler(
   delete req.body.OrderID;
   db.set(OrderID, req.body);
 
-  res.status(200).json({ status: "Success", data: db.get(OrderID) });
+  res.status(200).json({ status: "Success", data: db.JSON() });
 }
