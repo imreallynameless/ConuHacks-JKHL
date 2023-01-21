4eddf952f811c1ac27bb9bec698914f32cb04a52
meekranjer <logicism81@gmail.com>
2023-01-21 15:55:22 -0500

    Update

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -1,21 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
 import JSONdb from "simple-json-db";
-
-type Data = {
-  name: string;
-};
 
 // Temporary solution. Use something like 'redis' in the future
 const db = new JSONdb("db.json");
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const OrderID = req.body.OrderID;
-  delete req.body.OrderID;
-  db.set(OrderID, req.body);
+export default function handler(req, res) {
+  if (req.method == "POST") {
+    const OrderID = req.body.OrderID;
+    delete req.body.OrderID;
+    db.set(OrderID, req.body);
+    res.status(200).json({ status: "Success" });
+    return;
+  }
 
-  res.status(200).json({ status: "Success", data: db.JSON() });
+  res.status(200).json({ data: db.JSON() });
 }
