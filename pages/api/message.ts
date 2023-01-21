dab98f8d33f32fa31a23879a321b2affdb002d92
meekranjer <logicism81@gmail.com>
2023-01-21 14:20:58 -0500

    Using simple-json-db isntead

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -1,12 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import JSONdb from "simple-json-db";
 
 type Data = {
   name: string;
 };
 
 // Temporary solution. Use something like 'redis' in the future
-const buffer = {};
+const db = new JSONdb("db.json");
 
 export default function handler(
   req: NextApiRequest,
@@ -14,7 +15,7 @@ export default function handler(
 ) {
   const OrderID = req.body.OrderID;
   delete req.body.OrderID;
-  buffer[OrderID] = req.body;
+  db.set(OrderID, req.body);
 
-  res.status(200).json(buffer);
+  res.status(200).json({ status: "Success", data: db.get(OrderID) });
 }
